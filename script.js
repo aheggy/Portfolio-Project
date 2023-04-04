7220976ea40d0643590e4bcd2b03e233cf8ec6bf
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-04 06:25:22 -0400

    modify some changes on styling

--- script.js
+++ script.js
@@ -1,49 +1,14 @@
 
 let button = document.querySelector(".get-location")
 
+/*
+add event listener to the button in the home page 
+once it click it view the ISS current locatin on location page
+*/
 button.addEventListener("click", event =>{
-  // event.preventDefault()
   document.querySelector(".open-page").remove()
-  // localStorage.setItem('divRemoved', 'true')
   document.location.href = `location.html`
-  
-  // if (localStorage.getItem('divRemoved') === 'true') {
-  //   document.querySelector('.open-page').remove()
-  // }
-  
-  
-  // console.log("clicked")
-  
   
 })
 
-function enlargeImage(src) {
-  const viewImage = document.createElement("div");
-  const largeImage = document.createElement("img");
-  largeImage.src = src;
-  viewImage.appendChild(largeImage);
-  viewImage.style.position = 'fixed';
-  // viewImage.style.top = '40px';
-  // viewImage.style.left = '10%';
-  viewImage.style.width = 'auto';
-  viewImage.style.height = 'auto';
-  viewImage.style.background = 'rgba(0, 0, 0, 1)';
-  viewImage.style.display = 'flex';
-  viewImage.style.alignItems = 'center';
-  viewImage.style.justifyContent = 'center';
-  largeImage.style.maxWidth = '100vw';
-  largeImage.style.maxHeight = '100vh';
-  viewImage.addEventListener("click", () => {
-    viewImage.remove();
-  });
-  document.body.appendChild(viewImage);
-}
 
-const images = document.querySelectorAll("img");
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    enlargeImage(image.src);
-  });
-});
-
-// this section is for CONTACT US page
