19b231cc8670a0cdc688d949773d8cbac934b22a
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-04 06:38:19 -0400

    add some changes on the styling

--- fetchScrript.js
+++ fetchScrript.js
@@ -1,10 +1,12 @@
 
+// set the zoom and the center of the map
 function initMap() {
   let map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 0, lng: 0 },
     zoom: 2,
   });
   
+  //creating and icon to appear on the map for the location
   const marker = new google.maps.Marker({
     map, 
     draggable: false,
@@ -14,8 +16,8 @@ function initMap() {
     }
     });
 
-  //https://api.wheretheiss.at/v1/satellites/25544
-  //("http://api.open-notify.org/iss-now.json"
+
+  // Get the location of ISS every 5 seconds 
   setInterval(() => {
     fetch("https://api.wheretheiss.at/v1/satellites/25544")
     .then((response) => response.json())
@@ -29,6 +31,7 @@ function initMap() {
   }, 5000);
 }
 
+//Add script tag with the google maps API to html
 function loadScript() {
   const script = document.createElement('script');
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD4eUP4NTeLcy8A4YOkmP5z7IIbMDGX4N8';
