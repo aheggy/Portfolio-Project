4beb3d82a9716ec737ee9ee0dc19d40a3276ec30
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-04 15:48:50 -0400

    change the respose of sending message

--- contactScript.js
+++ contactScript.js
@@ -1,9 +1,22 @@
 const contactForm = document.getElementById("contact-form")
+const successMessage = document.getElementById("success-message");
+const errorMessage = document.getElementById("success-message");
 
 contactForm.addEventListener("submit", event =>{
-  event.preventDefault()
+    event.preventDefault()
+    const name = document.getElementById("name").value
+    const message = document.getElementById("message").value
+    
+    
 
-  const name = document.getElementById("name").value
-  alert(`Thank You for your message, ${name}. we will get back to you soon.`)
+//   alert(`Thank You for your message, ${name}. we will get back to you soon.`)
+if (name && message) {
+    successMessage.style.display = "block";
+    successMessage.innerText = "Message sent.";
+    setTimeout(() => {
+      successMessage.style.display = "none";
+      }, 3000);
+    } 
   contactForm.reset()
+  
 })
